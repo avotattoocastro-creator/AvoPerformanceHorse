a13fde29df9b0dc00fc7e0509116ea1d344fc8c6
avotattoocastro-creator <avo.tattoo.castro@gmail.com>
2026-05-27 08:46:34 +0200

    Update Package.swift

--- AVO.swiftpm/Package.swift
+++ AVO.swiftpm/Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "com.avoperformance.horse",
             teamIdentifier: "4J2D6P5BJX",
-            displayVersion: "1.1.1",
-            bundleVersion: "24",
+            displayVersion: "1.1.4",
+            bundleVersion: "27",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.brown),
             supportedDeviceFamilies: [
